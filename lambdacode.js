fe77f7e85d3cee0a7db095fbb518312313fc0bf4
nolim1t <nolimit@perceptionz.net>
2015-09-29 11:40:42 +0800

    First commit yo
    Processes payloads in the following format:
    
    ```javascript
    var lambdapayload = JSON.stringify({
        message: 'Hello World',
        custom: {
            senderid: 'testsenderid',
            recipientid: 'testrecipient',
            recipientname: 'Test Recipientname',
            messageid: 'testmsgid',
            devicetoken: 'token',
            deviceplatform: 'ios'
        }
    });
    ```
    
    ## To send
    ```javascript
    var sns = new AWS.SNS()
    var messagepayload = JSON.stringify({
        default: 'This is a default message',
        lambda: lambdapayload
    });
    var params = {
        Message: messagepayload,
        MessageStructure: 'json',
        TopicArn: 'arn:aws:sns:ap-northeast-1:518627074072:MessageQueueTopic'
    };
    sns.publish(params, function(err,data) {
        console.log(err);
        console.log(data);
    });
    ```

---
+++ lambdacode.js
@@ -0,0 +1,24 @@
+exports.handler = function(event, context) {
+    //console.log('Received event:', JSON.stringify(event, null, 2));
+    var parsed = JSON.parse(event.Records[0].Sns.Message);
+    if (parsed.message !== undefined) {
+        var processedMessage = {
+            
+        };
+        processedMessage.message = parsed.message;
+        console.log('Message received was: ' + processedMessage.message);
+        if (parsed.custom !== undefined) {
+            if (parsed.custom.senderid !== undefined) processedMessage.senderid = parsed.custom.senderid;
+            if (parsed.custom.recipientid !== undefined) processedMessage.recipientid = parsed.custom.recipientid;
+            if (parsed.custom.recipientname !== undefined) processedMessage.recipientname = parsed.custom.recipientname;
+            if (parsed.custom.deviceplatform !== undefined && parsed.custom.devicetoken !== undefined) {
+                processedMessage.deviceplatform = parsed.custom.deviceplatform;
+                processedMessage.devicetoken = parsed.custom.devicetoken;
+            }
+        }
+        console.log('processedMessage in JSON speak is: ' + JSON.stringify(processedMessage));
+        context.succeed('Done');
+    } else {
+        context.fail('Message key not found')
+    }
+};
